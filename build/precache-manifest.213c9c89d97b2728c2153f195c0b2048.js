ceb40a786bb610404aecb1eb9c56f802243350bc
Kaisokuvn <duongmilivn@gmail.com>
2020-05-08 15:35:44 +0200

    hello

---
+++ build/precache-manifest.213c9c89d97b2728c2153f195c0b2048.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19e305f340443bdcd000268ddccb70be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cfba3422ef537973b89",
+    "url": "/static/css/main.88976e0f.chunk.css"
+  },
+  {
+    "revision": "9e1e2e704830892161ee",
+    "url": "/static/js/2.b4e6ac7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4e6ac7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cfba3422ef537973b89",
+    "url": "/static/js/main.87901aad.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
